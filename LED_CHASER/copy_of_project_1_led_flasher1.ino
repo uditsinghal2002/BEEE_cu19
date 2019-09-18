8bba1732239214fcb1a170f697753a0d2e7192ca
uditsinghal2002 <53823547+uditsinghal2002@users.noreply.github.com>
2019-09-18 19:21:22 +0530

    Add files via upload

---
+++ LED_CHASER/copy_of_project_1_led_flasher1.ino
@@ -0,0 +1,46 @@
+
+void setup()
+{
+  
+  
+  pinMode(9,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(5,OUTPUT);
+  
+  pinMode(4,OUTPUT);
+}
+void loop()
+{
+  digitalWrite(9,HIGH);
+  delay(100);
+  digitalWrite(9,LOW);
+  delay(10);
+  
+  digitalWrite(8,HIGH);
+  delay(100);
+  digitalWrite(8,LOW);
+  delay(10);
+  
+  digitalWrite(7,HIGH);
+  delay(100);
+  digitalWrite(7,LOW);
+  delay(10);
+  
+  digitalWrite(6,HIGH);
+  delay(100);
+  digitalWrite(6,LOW);
+  delay(10);
+  
+  digitalWrite(5,HIGH);
+  delay(100);
+  digitalWrite(5,LOW);
+  delay(10);
+  
+  digitalWrite(4,HIGH);
+  delay(100);
+  digitalWrite(4,LOW);
+  delay(10);
+  
+}
